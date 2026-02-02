6a3993e37486c92f87bf264ba776bacbfc5486df
alf4ridzi <aalalfaridzi9@gmail.com>
2026-02-02 10:56:33 +0700

    banyak

---
+++ database/migrations/2026_02_02_031349_create_report_responses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Report;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("report_responses", function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Report::class, "report_id")->constrained();
+            $table
+                ->enum("status", ["done", "waiting", "proccess"])
+                ->default("waiting");
+            $table->text("feedback");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists("report_responses");
+    }
+};
